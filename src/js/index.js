2a2accab51ab133954de20009dfaddaceb636dfc
Vitalii Masliuhov <masliks456@gmail.com>
2022-09-01 06:12:19 +0300

    summary

--- src/js/index.js
+++ src/js/index.js
@@ -76,25 +76,25 @@ $(".carousel").slick({
    nextArrow:$('.next')})
 
 
-   (() => {
-    const refs = {
-      openModalBtn: document.querySelector('[data-modal-open]'),
-      openModalBtn1: document.querySelector('[data-modal-open-1]'),
-      openModalBtn2: document.querySelector('[data-modal-open-2]'),
-      closeModalBtn: document.querySelector('[data-modal-close]'),
-      modal: document.querySelector('[data-modal]'),
-      body: document.querySelector('[data-page]'),
-    };
+//    (() => {
+//     const refs = {
+//       openModalBtn: document.querySelector('[data-modal-open]'),
+//       openModalBtn1: document.querySelector('[data-modal-open-1]'),
+//       openModalBtn2: document.querySelector('[data-modal-open-2]'),
+//       closeModalBtn: document.querySelector('[data-modal-close]'),
+//       modal: document.querySelector('[data-modal]'),
+//       body: document.querySelector('[data-page]'),
+//     };
 
-    refs.openModalBtn.addEventListener('click', toggleModal);
-    refs.openModalBtn1.addEventListener('click', toggleModal);
-    refs.openModalBtn2.addEventListener('click', toggleModal);
-    refs.closeModalBtn.addEventListener('click', toggleModal);
+//     refs.openModalBtn.addEventListener('click', toggleModal);
+//     refs.openModalBtn1.addEventListener('click', toggleModal);
+//     refs.openModalBtn2.addEventListener('click', toggleModal);
+//     refs.closeModalBtn.addEventListener('click', toggleModal);
 
-    function toggleModal() {
-      refs.modal.classList.toggle('backdrop--is-hiden');
-      document.body.classList.toggle('modal-open');
-    }
-  })();
+//     function toggleModal() {
+//       refs.modal.classList.toggle('backdrop--is-hiden');
+//       document.body.classList.toggle('modal-open');
+//     }
+//   })();
 
 
