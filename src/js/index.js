922e454756c66e6d9d96dfc823b003fa00776c64
Vitaliy Meshechek <meshechek.vitaliy@ukr.net>
2022-09-01 03:05:17 +0300

    js

--- src/js/index.js
+++ src/js/index.js
@@ -74,3 +74,26 @@ $(".carousel").slick({
    arrows:true,
    prevArrow: $('.prev'),
    nextArrow:$('.next')})
+
+
+   (() => {
+    const refs = {
+      openModalBtn: document.querySelector('[data-modal-open]'),
+      openModalBtn1: document.querySelector('[data-modal-open-1]'),
+      openModalBtn2: document.querySelector('[data-modal-open-2]'),
+      closeModalBtn: document.querySelector('[data-modal-close]'),
+      modal: document.querySelector('[data-modal]'),
+      body: document.querySelector('[data-page]'),
+    };
+
+    refs.openModalBtn.addEventListener('click', toggleModal);
+    refs.openModalBtn1.addEventListener('click', toggleModal);
+    refs.openModalBtn2.addEventListener('click', toggleModal);
+    refs.closeModalBtn.addEventListener('click', toggleModal);
+
+    function toggleModal() {
+      refs.modal.classList.toggle('backdrop--is-hiden');
+      document.body.classList.toggle('modal-open');
+    }
+  })();
+
