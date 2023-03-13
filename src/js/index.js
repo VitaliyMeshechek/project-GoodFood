b5d71ecd3f588c448075866a1fa72a74b0b72484
Vitaliy Meshechek <meshechek.vitaliy@ukr.net>
2023-03-13 15:50:42 +0200

    add modal-menu

--- src/js/index.js
+++ src/js/index.js
@@ -1,77 +1,77 @@
-!function(e){"function"!=typeof e.matches&&(e.matches=e.msMatchesSelector||e.mozMatchesSelector||e.webkitMatchesSelector||function(e){for(var t=this,o=(t.document||t.ownerDocument).querySelectorAll(e),n=0;o[n]&&o[n]!==t;)++n;return Boolean(o[n])}),"function"!=typeof e.closest&&(e.closest=function(e){for(var t=this;t&&1===t.nodeType;){if(t.matches(e))return t;t=t.parentNode}return null})}(window.Element.prototype);
+// !function(e){"function"!=typeof e.matches&&(e.matches=e.msMatchesSelector||e.mozMatchesSelector||e.webkitMatchesSelector||function(e){for(var t=this,o=(t.document||t.ownerDocument).querySelectorAll(e),n=0;o[n]&&o[n]!==t;)++n;return Boolean(o[n])}),"function"!=typeof e.closest&&(e.closest=function(e){for(var t=this;t&&1===t.nodeType;){if(t.matches(e))return t;t=t.parentNode}return null})}(window.Element.prototype);
 
 
-document.addEventListener('DOMContentLoaded', function() {
+// document.addEventListener('DOMContentLoaded', function() {
 
-   /* Записываем в переменные массив элементов-кнопок и подложку.
-      Подложке зададим id, чтобы не влиять на другие элементы с классом overlay*/
-   var modalButtons = document.querySelectorAll('.js-open-modal'),
-       overlay      = document.querySelector('.js-overlay-modal'),
-       closeButtons = document.querySelectorAll('.js-modal-close');
-
-
-   /* Перебираем массив кнопок */
-   modalButtons.forEach(function(item){
-
-      /* Назначаем каждой кнопке обработчик клика */
-      item.addEventListener('click', function(e) {
-
-         /* Предотвращаем стандартное действие элемента. Так как кнопку разные
-            люди могут сделать по-разному. Кто-то сделает ссылку, кто-то кнопку.
-            Нужно подстраховаться. */
-         e.preventDefault();
-
-         /* При каждом клике на кнопку мы будем забирать содержимое атрибута data-modal
-            и будем искать модальное окно с таким же атрибутом. */
-         var modalId = this.getAttribute('data-modal-menu'),
-             modalElem = document.querySelector('.modal[data-modal-menu="' + modalId + '"]');
+//    /* Записываем в переменные массив элементов-кнопок и подложку.
+//       Подложке зададим id, чтобы не влиять на другие элементы с классом overlay*/
+//    var modalButtons = document.querySelectorAll('.js-open-modal'),
+//        overlay      = document.querySelector('.js-overlay-modal'),
+//        closeButtons = document.querySelectorAll('.js-modal-close');
 
 
-         /* После того как нашли нужное модальное окно, добавим классы
-            подложке и окну чтобы показать их. */
-         modalElem.classList.add('active');
-         overlay.classList.add('active');
-      }); // end click
+//    /* Перебираем массив кнопок */
+//    modalButtons.forEach(function(item){
 
-   }); // end foreach
+//       /* Назначаем каждой кнопке обработчик клика */
+//       item.addEventListener('click', function(e) {
 
+//          /* Предотвращаем стандартное действие элемента. Так как кнопку разные
+//             люди могут сделать по-разному. Кто-то сделает ссылку, кто-то кнопку.
+//             Нужно подстраховаться. */
+//          e.preventDefault();
 
-   closeButtons.forEach(function(item){
-
-      item.addEventListener('click', function(e) {
-         var parentModal = this.closest('.modal');
-
-         parentModal.classList.remove('active');
-         overlay.classList.remove('active');
-      });
-
-   }); // end foreach
+//          /* При каждом клике на кнопку мы будем забирать содержимое атрибута data-modal
+//             и будем искать модальное окно с таким же атрибутом. */
+//          var modalId = this.getAttribute('data-modal-menu'),
+//              modalElem = document.querySelector('.modal[data-modal-menu="' + modalId + '"]');
 
 
-    document.body.addEventListener('keyup', function (e) {
-        var key = e.keyCode;
+//          /* После того как нашли нужное модальное окно, добавим классы
+//             подложке и окну чтобы показать их. */
+//          modalElem.classList.add('active');
+//          overlay.classList.add('active');
+//       }); // end click
 
-        if (key == 27) {
-
-            document.querySelector('.modal.active').classList.remove('active');
-            document.querySelector('.overlay').classList.remove('active');
-        };
-    }, false);
+//    }); // end foreach
 
 
-    overlay.addEventListener('click', function() {
-        document.querySelector('.modal.active').classList.remove('active');
-        this.classList.remove('active');
-    });
+//    closeButtons.forEach(function(item){
 
-}); // end ready
+//       item.addEventListener('click', function(e) {
+//          var parentModal = this.closest('.modal');
+
+//          parentModal.classList.remove('active');
+//          overlay.classList.remove('active');
+//       });
+
+//    }); // end foreach
 
 
-// // slider
-import $ from "jquery";
-import "slick-carousel";
-$(".carousel").slick({
-   arrows:true,
-   prevArrow: $('.prev'),
-   nextArrow:$('.next')})
+//     document.body.addEventListener('keyup', function (e) {
+//         var key = e.keyCode;
+
+//         if (key == 27) {
+
+//             document.querySelector('.modal.active').classList.remove('active');
+//             document.querySelector('.overlay').classList.remove('active');
+//         };
+//     }, false);
+
+
+//     overlay.addEventListener('click', function() {
+//         document.querySelector('.modal.active').classList.remove('active');
+//         this.classList.remove('active');
+//     });
+
+// }); // end ready
+
+
+// // // slider
+// import $ from "jquery";
+// import "slick-carousel";
+// $(".carousel").slick({
+//    arrows:true,
+//    prevArrow: $('.prev'),
+//    nextArrow:$('.next')})
 
--- src/js/modal.js
+++ src/js/modal.js
@@ -4,11 +4,13 @@
     openModalBtn1: document.querySelector('[data-modal-open-1]'),
     openModalBtn2: document.querySelector('[data-modal-open-2]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
+    modalMenu: document.querySelector('[data-modal-menu]'),
     modal: document.querySelector('[data-modal]'),
     body: document.querySelector('[data-page]'),
+    bodyMenu: document.querySelector('[data-page-menu]'),
   };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.openModalBtn.addEventListener('click', openModalMenu);
   refs.openModalBtn1.addEventListener('click', toggleModal);
   refs.openModalBtn2.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
@@ -16,5 +18,10 @@
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
     refs.body.classList.toggle('modal-open');
+  }
+
+  function openModalMenu() {
+    refs.modalMenu.classList.toggle('is-hidden-menu');
+    refs.bodyMenu.classList.toggle('modal-open-menu');
   }
 })();
